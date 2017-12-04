4ec0d53edc6918dfaee967cabdc9506ddd89d807
Vernon Reis <32598819+venom9122@users.noreply.github.com>
2017-12-04 10:04:18 -0800

    added in api query and added api key access

--- controllers/user.js
+++ controllers/user.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const router  = express.Router();
 const bcrypt = require('bcrypt');
+const request = require('request');
 
 const User = require('../models/user.js');
 
 
+// My personal Steam API access Key
+const apiKey = process.env.APIKEY || require("../apiKey.js");
 
 router.get('/', async (req,res) => {
-    const allUsers = await User.find();
-    res.send(allUsers);
+    allUsers = request("http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=" + apiKey+"&steamid=76561197960434622&format=json", (error,response,body) => {
+        test = JSON.parse(body);
+        res.send(test);
+    });
+    // res.send(allUsers);
 });
 
 
